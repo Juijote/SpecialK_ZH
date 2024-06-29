10796caaa125590337f97635747249e258dd88ab
Juijote <Juijote@outlook.com>
2024-06-30 01:14:19 +0800

    up v24.6.21

--- src/control_panel.cpp
+++ src/control_panel.cpp
@@ -787,7 +787,7 @@ SK_ImGui_ControlPanelTitle (void)
   {
     title += "Special K [v ";
     title += SK_GetVersionStrA ();
-    title += "] [Juij 汉化] [2024-06-09]";
+    title += "] [Juij 汉化] [2024-06-21]";
   }
 
   title += "  控制面板";
